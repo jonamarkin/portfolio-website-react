028592a36740f55edd26eb8b76d2fd9e025f3ded
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-09 13:44:28 +0200

    useEffect initialized Aos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Aos from "aos";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return <h1 className="text-indigo-600 text-2xl font-[500]">Hello World</h1>;
 }
 
